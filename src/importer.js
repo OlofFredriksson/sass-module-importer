f03a17a9fe815842a44efc620d5c36a2b2b37bf3
Olof Fredriksson <oloflolof@gmail.com>
2025-02-08 19:07:41 +0100

    feat: wip

--- src/importer.js
+++ src/importer.js
@@ -1,6 +1,10 @@
 import path from "node:path";
+import fs from "node:fs";
 import { pathToFileURL } from "node:url";
 import { createRequire } from "node:module";
+import resolvePackagePath from "resolve-package-path";
+import { exports, legacy } from "resolve.exports";
+import { getPackageNameFromPath } from "./parsePackageName";
 
 const require = createRequire(import.meta.url);
 const WEBPACK_NODE_MODULE_PREFIX = "~";
@@ -11,9 +15,45 @@ export const moduleImporter = {
             findUrl = url.substring(1);
         }
 
-        const directory = path.dirname(findUrl);
-        const fileName = path.basename(findUrl);
+        const packageName = getPackageNameFromPath(findUrl);
 
+        const fileName = findUrl.split(packageName)[1];
+        const packagePath = resolvePackagePath(packageName, process.cwd());
+
+        /* Validate if existing package */
+        if (!packagePath) {
+            return null;
+        }
+
+        const packageJson = JSON.parse(
+            fs.readFileSync(packagePath, { encoding: "utf-8" }),
+        );
+
+        const directory = path.dirname(packagePath);
+
+        /* Check exports */
+        try {
+            const match = exports(packageJson, fileName, {
+                conditions: ["sass"],
+            });
+
+            if (match && match.length === 1) {
+                return new URL(pathToFileURL(path.join(directory, match[0])));
+            }
+        } catch {
+            /* empty */
+        }
+
+        /* Check main fields */
+        const match = legacy(packageJson, { fields: ["sass", "main"] });
+        console.log(match);
+
+        if (match) {
+            console.log(path.join(directory, match));
+            return new URL(pathToFileURL(path.join(directory, match)));
+        }
+
+        /* Direct link */
         const search = [
             `${fileName}.css`,
             `${fileName}.scss`,
@@ -24,9 +64,7 @@ export const moduleImporter = {
         for (const variant of search) {
             try {
                 const moduleName = path.posix.join(directory, variant);
-                const resolved = require.resolve(moduleName, {
-                    paths: [process.cwd()],
-                });
+                const resolved = require.resolve(moduleName);
                 return new URL(pathToFileURL(resolved));
             } catch (err) {
                 if (err.code !== "MODULE_NOT_FOUND") {
--- src/importer.spec.js
+++ src/importer.spec.js
@@ -1,15 +1,62 @@
 import fs from "node:fs";
 import process from "node:process";
+
 import { compileString } from "sass";
 import { jest } from "@jest/globals";
 import { moduleImporter } from "./importer";
 
-it("should be able to transform scss", () => {
+it("should be able to transform scss using package without exports and main fields", () => {
     const spyProcess = jest.spyOn(process, "cwd");
-    spyProcess.mockReturnValue("../fixtures/");
+
+    spyProcess.mockReturnValue("./fixtures");
 
     const result = compileString(
         fs.readFileSync("./fixtures/main.scss", "utf-8"),
+        {
+            style: "expanded",
+            importers: [moduleImporter],
+        },
+    );
+    expect(result.css).toMatchSnapshot();
+});
+
+it("should be able to transform scss using package with exports", () => {
+    const spyProcess = jest.spyOn(process, "cwd");
+
+    spyProcess.mockReturnValue("./fixtures");
+
+    const result = compileString(
+        fs.readFileSync("./fixtures/exports.scss", "utf-8"),
+        {
+            style: "expanded",
+            importers: [moduleImporter],
+        },
+    );
+    expect(result.css).toMatchSnapshot();
+});
+
+it("should be able to transform scss using package with main field", () => {
+    const spyProcess = jest.spyOn(process, "cwd");
+
+    spyProcess.mockReturnValue("./fixtures");
+
+    const result = compileString(
+        fs.readFileSync("./fixtures/mainField.scss", "utf-8"),
+        {
+            style: "expanded",
+            importers: [moduleImporter],
+        },
+    );
+    expect(result.css).toMatchSnapshot();
+});
+
+it("should be able to transform scss using package with sass fields", () => {
+    const spyProcess = jest.spyOn(process, "cwd");
+
+    spyProcess.mockReturnValue("./fixtures");
+
+    const result = compileString(
+        fs.readFileSync("./fixtures/sassField.scss", "utf-8"),
         {
             style: "expanded",
             importers: [moduleImporter],
